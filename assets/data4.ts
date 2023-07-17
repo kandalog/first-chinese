a9e3b4ec4524bdfb45a6646f905c0b83577a926f
kandalog <asuasu.tee@gmail.com>
2023-07-17 12:45:06 +0900

    修正

--- assets/data4.ts
+++ assets/data4.ts
@@ -1,55 +1,7 @@
-export const pinyin4 = [
-  "shéi",
-  "nǎ'er",
-  "shénme",
-  "zěnme",
-  "zěnme yàng",
-  "duōshǎo qián",
-  "jǐ yuè jǐ hòu",
-  "xīngqí jǐ",
-  "shénme shíhòu",
-  "duō cháng shíjiān",
-  "jǐ diǎn",
-];
+export const pinyin4 = [];
 
-export const en4 = [
-  "shéi",
-  "nǎ'er",
-  "shénme",
-  "zěnme",
-  "zěnme yàng",
-  "duōshǎo qián",
-  "jǐ yuè jǐ hòu",
-  "xīngqí jǐ",
-  "shénme shíhòu",
-  "duō cháng shíjiān",
-  "jǐ diǎn",
-];
+export const en4 = [];
 
-export const ch4 = [
-  "谁",
-  "哪儿",
-  "什么",
-  "怎么",
-  "怎么样",
-  "多少钱",
-  "几月几后",
-  "星期几",
-  "什么时候",
-  "多长时间",
-  "几点",
-];
+export const ch4 = [];
 
-export const jp4 = [
-  "だれ",
-  "どこ",
-  "なに",
-  "どうやって、 なぜ",
-  "どうですか",
-  "いくら",
-  "何月何日",
-  "何曜日",
-  "いつ",
-  "どれくらい",
-  "何時",
-];
+export const jp4 = [];
